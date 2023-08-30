0301f63c717c2723a7963ce314651b1352634dab
mavr1ke <mainstip@mail.ru>
2023-08-30 15:39:39 +0200

    update hw32

--- lesson_32/code/homework_32/shapetest/ShapeTest.java
+++ lesson_32/code/homework_32/shapetest/ShapeTest.java
@@ -1,5 +1,6 @@
 package homework_32.shapetest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +10,7 @@ class ShapeTest {
     Shape[] shapes;
 
     @Test
+    @DisplayName("Check work with array of same type figures")
     public void severalFiguresOfSameType_totalSquare_test() {
         shapes = new Shape[2];
         shapes[0] = new Rectangle(5, 10);
@@ -17,6 +19,7 @@ class ShapeTest {
     }
 
     @Test
+    @DisplayName("Check work with array of different type figures")
     public void severalFiguresOfDifferentType_totalSquare_test() {
         shapes = new Shape[3];
         shapes[0] = new Rectangle(5, 10);
@@ -26,12 +29,14 @@ class ShapeTest {
     }
 
     @Test
+    @DisplayName("Check work with empty array")
     public void zeroFigures_totalSquare_test() {
         shapes = new Shape[0];
         assertEquals(0, shapes[0].totalSquare(shapes));
     }
 
     @Test
+    @DisplayName("Check work with array of only one figure")
     public void oneFigure_totalSquare_test() {
         shapes = new Shape[1];
         shapes[0] = new Rectangle(5, 10);
