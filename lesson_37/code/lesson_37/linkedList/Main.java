5cab168d1d3756d04883ee783cceb836ae6807dc
mavr1ke <mainstip@mail.ru>
2023-09-06 13:01:33 +0200

    add lesson37

---
+++ lesson_37/code/lesson_37/linkedList/Main.java
@@ -0,0 +1,9 @@
+package lesson_37.linkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+    }
+}
