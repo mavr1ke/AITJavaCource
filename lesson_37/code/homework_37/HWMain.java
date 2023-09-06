a527c54b18515a4b9feb950f1170b40efd6f54ef
mavr1ke <mainstip@mail.ru>
2023-09-06 21:08:31 +0200

    add hw37

--- lesson_37/code/homework_37/HWMain.java
+++ lesson_37/code/homework_37/HWMain.java
@@ -1,41 +1,59 @@
 package homework_37;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class HWMain {
 
-    public static String shortestOrLongestString(List<String> list) {
+    public static String shortestOrLongestString(LinkedList<String> list) {
         if (list == null || list.isEmpty()) {
             throw new IllegalArgumentException("List of strings is empty");
         }
-        int maxIndex = 0, minIndex = 0;
 
-        String max = list.get(0);
-        String min = list.get(0);
+        String shortest = list.getFirst();
+        String longest = list.getFirst();
 
-        for (String str : list) {
-            if (str.length() > max.length()) {
-                max = str;
-                maxIndex = list.indexOf(str);
-            } else if (str.length() < min.length()) {
-                min = str;
-                minIndex = list.indexOf(str);
+        ListIterator<String> iterator = list.listIterator();
+        int index = 0, longestIndex = 0, shortestIndex = 0;
+
+        while (iterator.hasNext()) {
+            String current = iterator.next();
+            if (current.length() > longest.length()) {
+                longest = current;
+                longestIndex = index;
+            } else if (current.length() < shortest.length()) {
+                shortest = current;
+                shortestIndex = index;
             }
+            index++;
         }
 
-        return list.get(Math.min(maxIndex, minIndex));
+        return list.get(Math.min(longestIndex, shortestIndex));
     }
 
     public static void main(String[] args) {
 
-        List<String> list = Arrays.asList("One", "Two", "Three", "Fo", "Five");
+        LinkedList<String> list = new LinkedList<>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Fo");
+        list.add("Five");
+
         System.out.println(HWMain.shortestOrLongestString(list));
 
-        List<String> testList1 = Arrays.asList("Apple", "Banana", "Cherry", "Date");
+        LinkedList<String> testList1 = new LinkedList<>();
+        testList1.add("Apple");
+        testList1.add("Banana");
+        testList1.add("Cherry");
+        testList1.add("Date");
         System.out.println(HWMain.shortestOrLongestString(testList1));
 
-        List<String> testList2 = Arrays.asList("Red", "Green", "Blue", "Yellow");
+        LinkedList<String> testList2 = new LinkedList<>();
+        testList2.add("Red");
+        testList2.add("Green");
+        testList2.add("Blue");
+        testList2.add("Yellow");
         System.out.println(HWMain.shortestOrLongestString(testList2));
     }
 }
