3379e12311418b5e5186d20ac12915b1c4738d13
mavr1ke <mainstip@mail.ru>
2023-09-11 22:50:59 +0200

    update hw40

--- lesson_40/code/homework_40/HWMain.java
+++ lesson_40/code/homework_40/HWMain.java
@@ -4,24 +4,16 @@ import java.util.*;
 
 public class HWMain {
 
-        /*
-         С помощью множеств реализовать метод, который принимает лист имен и возвращает лист без повторяющихся элементов
-        */
+//  С помощью множеств реализовать метод, который принимает лист имен и возвращает лист без повторяющихся элементов
 
     public static List<String> deleteRepeatingElements(List<String> list) {
-        if (list.isEmpty()) {
-            throw new IllegalArgumentException("List is empty");
-        }
         Set<String> linkedHashSetNames = new LinkedHashSet<>(list);
-        list.clear();
-        list.addAll(linkedHashSetNames);
-        return list;
+        return new ArrayList<>(linkedHashSetNames);
     }
 
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
         String[] arrayOfNames = {"Bill", "Jack", "John", "Bill", "Ann"};
-        Collections.addAll(names, arrayOfNames);
+        List<String> names = new ArrayList<>(Arrays.asList(arrayOfNames));
         System.out.println(names);
         System.out.println(HWMain.deleteRepeatingElements(names));
     }
