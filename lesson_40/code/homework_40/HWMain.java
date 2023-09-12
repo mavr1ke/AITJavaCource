00d7450dbb1acc3ad8798f192bd95a5fb7640051
mavr1ke <mainstip@mail.ru>
2023-09-12 12:26:56 +0200

    update hw40

--- lesson_40/code/homework_40/HWMain.java
+++ lesson_40/code/homework_40/HWMain.java
@@ -11,10 +11,31 @@ public class HWMain {
         return new ArrayList<>(linkedHashSetNames);
     }
 
+    public static List<String> getDuplicates(List<String> list) {
+//        Set<String> linkedHashSet = new LinkedHashSet<>(list);
+//        for (String string: linkedHashSet) {
+//            list.remove(string);
+//        }
+//        linkedHashSet.clear();
+//        linkedHashSet.addAll(list);
+
+        Set<String> uniqueElements = new LinkedHashSet<>();
+        Set<String> duplicateElements = new LinkedHashSet<>();
+
+        for (String element : list) {
+            if (!uniqueElements.add(element)) {
+                duplicateElements.add(element);
+            }
+        }
+
+        return new ArrayList<>(duplicateElements);
+    }
+
     public static void main(String[] args) {
-        String[] arrayOfNames = {"Bill", "Jack", "John", "Bill", "Ann"};
+        String[] arrayOfNames = {"Bill", "Jack", "John", "Bill", "Ann", "Bill", "Ann", "Ann"};
         List<String> names = new ArrayList<>(Arrays.asList(arrayOfNames));
         System.out.println(names);
         System.out.println(HWMain.deleteRepeatingElements(names));
+        System.out.println(HWMain.getDuplicates(names));
     }
 }
