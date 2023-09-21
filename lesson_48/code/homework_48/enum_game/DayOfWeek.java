31fe7f357e435273979aefe6dfecd514e82d6d61
mavr1ke <mainstip@mail.ru>
2023-09-22 00:49:10 +0200

    add hw48

---
+++ lesson_48/code/homework_48/enum_game/DayOfWeek.java
@@ -0,0 +1,12 @@
+package homework_48.enum_game;
+
+public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+
+}
---
+++ lesson_48/code/homework_48/enum_game/DayOfWeekResolver.java
@@ -0,0 +1,65 @@
+package homework_48.enum_game;
+
+import java.util.Scanner;
+
+public class DayOfWeekResolver {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static String stringFromScanner(String day) {
+        if (day.isEmpty()) {
+            throw new NullPointerException("String with day is empty");
+        }
+        return day;
+    }
+
+    private static DayOfWeek convertToDayOfWeek(String day) {
+        try {
+            return DayOfWeek.valueOf(day.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Incorrect day of the week: " + day, e);
+        }
+    }
+
+    private static boolean isWeekend(DayOfWeek day) {
+        switch (day) {
+            case MONDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+            case THURSDAY:
+            case FRIDAY: {
+                return false;
+            }
+            case SATURDAY:
+            case SUNDAY: {
+                return true;
+            }
+            default:
+                return false;
+        }
+    }
+
+    public static void start() {
+        boolean continueGame = true;
+        while (continueGame) {
+            System.out.println("\nEnter the day (or type 'exit' to quit): ");
+            String day = scanner.nextLine().trim();
+            if (day.equalsIgnoreCase("exit")) {
+                continueGame = false;
+            } else {
+                System.out.println("Is '" + day + "' a weekend?");
+                try {
+                    boolean res = isWeekend(convertToDayOfWeek(stringFromScanner(day)));
+                    if (res) {
+                        System.out.println("Yes");
+                    } else {
+                        System.out.println("No");
+                    }
+                } catch (IllegalArgumentException | NullPointerException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        scanner.close();
+    }
+}
