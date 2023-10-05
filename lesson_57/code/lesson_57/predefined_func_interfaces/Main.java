27d8c70bafe0dea97d8bb67456d7de66ba67c0b9
mavr1ke <mainstip@mail.ru>
2023-10-05 22:24:50 +0300

    update lesson57

--- lesson_57/code/lesson_57/predefined_func_interfaces/Main.java
+++ lesson_57/code/lesson_57/predefined_func_interfaces/Main.java
@@ -1,5 +1,6 @@
 package lesson_57.predefined_func_interfaces;
 
+import java.util.Random;
 import java.util.function.*;
 
 public class Main {
@@ -15,6 +16,7 @@ public class Main {
         System.out.println(function.apply(10));
 
         Function<Integer, String> function1 = num -> num + " times";
+        System.out.println(function1.apply(2));
 
         BinaryOperator<Integer> sum = Integer::sum;
         System.out.println(sum.apply(1, 2));
@@ -25,20 +27,34 @@ public class Main {
         /*
         Supplier<T> - не принимает ничего, возращает обьект типа Т
         его метод T get()
-        TODO: придумать пример
         */
+        Supplier<Integer> randomSupplier = () -> new Random().nextInt(100);
+        System.out.println(randomSupplier.get());
 
         /*
         BiFunction<T,U,R> - принимает параметры типа T,U , возращает обьект типа R
         метод apply
+        BiFunction<Integer,Integer,Integer> biFunction = Integer::sum;
         */
-//        BiFunction<Integer,Integer,Integer> biFunction = Integer::sum;
-//        TODO: придумать пример
+        BiFunction<Integer, Integer, Integer> sumFunction = Integer::sum;
+        int result = sumFunction.apply(5, 7);
+        System.out.println(result);
 
         Predicate<Integer> age18 = num -> num > 18;
         Predicate<Integer> age80 = num -> num < 80;
         System.out.println(age18.and(age80).test(12));
         System.out.println(age18.and(age80).test(40));
         System.out.println(age18.and(age80).test(90));
+
+        Predicate<String> containsJava = str -> str.contains("Java");
+        Predicate<String> containsHello = str -> str.contains("Hello");
+
+        System.out.println(containsJava.or(containsHello).test("hello java"));
+        System.out.println(containsJava.or(containsHello).test("java"));
+        System.out.println(containsJava.or(containsHello).test("hello world"));
+        System.out.println(containsJava.or(containsHello).test("world"));
+
+        Predicate<Integer> age1 = num -> num > 18;
+        System.out.println(age1.negate().test(19));
     }
 }
